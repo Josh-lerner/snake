0af1cc8fe402fa164d2a10e2a6b8b058ad4c2b70
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-04 16:01:32 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,17 +1,19 @@
 const net = require('net');
+const {IP}= require('./constants');
+const {PORT}= require('./constants');
+
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   conn.setEncoding('utf8'); 
 
   conn.on('connect', () => console.log('Successfully connected to game server'));  
   conn.on('connect', () => conn.write('Name: JEL'));
   
-  //  conn.on('connect', () => conn.write("Move: up"))
+  
   conn.on('data', (data) => console.log('Server says: ', data));  // handler for data
-
 
   return conn;
 }
