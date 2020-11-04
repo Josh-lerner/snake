ab5b70b8d1f0cc0eb9961b4d538b37c7657895ec
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-04 15:28:48 -0500

    Update input.js

--- input.js
+++ input.js
@@ -1,18 +1,30 @@
-const setupInput = function() {
+
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-
-  const handleUserInput = function (){
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    } process.stdout.write('.');
-  });
-  }
- handleUserInput()
-  return stdin;
+  stdin.on('data', handleUserInput) // callback handleUserInpur... async
+  return stdin
 }
 
-module.exports = setupInput;
+
+const handleUserInput = ('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up")
+  } else if (key === "a") {
+    connection.write("Move: left")
+  } else if (key === "s") {
+    connection.write("Move: down")
+  } else if (key === "d") {
+    connection.write("Move: right")
+  }
+});
+
+
+module.exports = {setupInput};
