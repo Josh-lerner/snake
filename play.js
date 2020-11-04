8039781252a05486af40c4f9c21871aa65fbe4cd
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-04 14:13:52 -0500

    Update play.js

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-const connect = require('./client')
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = function (){
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    } process.stdout.write('.');
-  });
-  }
- handleUserInput()
-  return stdin;
-}
-
+const  connect = require('./client')
+const setupInput = require('./input')
 
 console.log('Connecting ...');
 connect();
+
 setupInput();
